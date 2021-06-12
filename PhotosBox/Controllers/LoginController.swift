3488268766fef58b3b8b416d3ffbc072029e4b3e
Min Thet Maung <minthetmaung@nexlabs.co>
2021-06-12 16:18:00 +0630

    conenct outlets on login and signup controllers

---
+++ PhotosBox/Controllers/LoginController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginController.swift
+//  PhotosBox
+//
+//  Created by Min Thet Maung on 12/06/2021.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageTextView: UITextView!
+    @IBOutlet weak var loginButton: RoundedButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
---
+++ PhotosBox/Controllers/SignupController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupController.swift
+//  PhotosBox
+//
+//  Created by Min Thet Maung on 12/06/2021.
+//
+
+import UIKit
+
+class SignupController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageTextView: UITextView!
+    @IBOutlet weak var signupButton: RoundedButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func signupButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+
+
+}
